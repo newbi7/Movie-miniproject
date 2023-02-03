a07ba489f0eb14e4bf6caae22faafcf3d62f7280
Newbi7 <anelitgel12@gmail.com>
2023-02-03 10:57:14 +0900

    start

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <mainPage></mainPage>
+  </div>
+</template>
+
+<script>
+import mainPage from "./componnents/mainPage.vue";
+
+export default {
+  components: {
+    mainPage,
+  },
+  methods: {},
+};
+</script>
+
+<style>
+</style>
